ad3e28ca7d3682f7f13f554b73b469ebf3680bcc
zyar3k <a.zygan@gmail.com>
2023-01-18 11:06:53 +0100

    Finish drwing map

--- game.js
+++ game.js
@@ -9,6 +9,11 @@ let createRect = (x, y, width, height, color) => {
 };
 
 let fps = 30;
+let oneBlockSize = 20;
+let wallColor = "#6a6a6a";
+let wallSpaceWidth = oneBlockSize / 1.3;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
 
 let map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -21,14 +26,14 @@ let map = [
   [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
   [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
   [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
-  [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+  [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
   [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
   [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
   [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
@@ -46,7 +51,65 @@ let update = () => {
 };
 
 let draw = () => {
+  createRect(0, 0, canvas.width, canvas.height, "black");
   //   TODO!
+  drawWalls();
 };
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+let drawWalls = () => {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] == 1) {
+        // walls
+        {
+          createRect(
+            j * oneBlockSize,
+            i * oneBlockSize,
+            oneBlockSize,
+            oneBlockSize,
+            wallColor
+          );
+          if (j > 0 && map[i][j - 1] == 1) {
+            createRect(
+              j * oneBlockSize,
+              i * oneBlockSize + wallOffset,
+              wallSpaceWidth + wallOffset,
+              wallSpaceWidth,
+              wallInnerColor
+            );
+          }
+          if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+            createRect(
+              j * oneBlockSize + wallOffset,
+              i * oneBlockSize + wallOffset,
+              wallSpaceWidth + wallOffset,
+              wallSpaceWidth,
+              wallInnerColor
+            );
+          }
+
+          if (i > 0 && map[i - 1][j] == 1) {
+            createRect(
+              j * oneBlockSize + wallOffset,
+              i * oneBlockSize,
+              wallSpaceWidth,
+              wallSpaceWidth + wallOffset,
+              wallInnerColor
+            );
+          }
+          if (i < map.length - 1 && map[i + 1][j] == 1) {
+            createRect(
+              j * oneBlockSize + wallOffset,
+              i * oneBlockSize + wallOffset,
+              wallSpaceWidth,
+              wallSpaceWidth + wallOffset,
+              wallInnerColor
+            );
+          }
+        }
+      }
+    }
+  }
+};
