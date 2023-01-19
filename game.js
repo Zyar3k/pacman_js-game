955a26e4d13498f7a5d34ec936a077b875f0367f
zyar3k <a.zygan@gmail.com>
2023-01-19 21:06:36 +0100

    Add ghost moving and looking for a packman

--- game.js
+++ game.js
@@ -57,6 +57,13 @@ let map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+let randomTargetsForGhosts = [
+  { x: 1 * oneBlockSize, y: 1 * oneBlockSize },
+  { x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+  { x: (map[0].length - 2) * oneBlockSize, y: oneBlockSize },
+  { x: (map[0].length - 2) * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+];
+
 let gameLoop = () => {
   update();
   draw();
@@ -65,6 +72,9 @@ let gameLoop = () => {
 let update = () => {
   pacman.moveProcess();
   pacman.eat();
+  for (let i = 0; i < ghosts.length; i++) {
+    ghosts[i].moveProcess();
+  }
 };
 
 let drawFoods = () => {
